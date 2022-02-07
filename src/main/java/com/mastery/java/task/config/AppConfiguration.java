9533f21c1caefdbae76ebce6efec2b716513b513
Dzianis <tdenis.20012510@gmail.com>
2022-02-07 21:38:17 +0300

    Start

--- src/main/java/com/mastery/java/task/config/AppConfiguration.java
+++ src/main/java/com/mastery/java/task/config/AppConfiguration.java
@@ -1,4 +1,7 @@
 package com.mastery.java.task.config;
 
 public class AppConfiguration {
+    public static void main(String[] args){
+        System.out.println("Welcome to Godel Task!");
+    }
 }
